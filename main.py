44e537335e99ca50b394399ef27b235ac7e5fc37
BailangMax <langb6416@gmail.com>
2026-07-27 19:51:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ PORT = int(os.environ.get('PORT', 7860))
 NEZHA_SERVER = os.environ.get('NEZHA_SERVER', 'nz.xry.kdns.fr:443')
 NEZHA_PORT = os.environ.get('NEZHA_PORT', '')
 NEZHA_KEY = os.environ.get('NEZHA_KEY', 'oZEaed4hQmmjuTBzga1vE5HlUPFbYvCw')
-UUID = os.environ.get('UUID', '1f6726cf-39da-4372-82ca-5b60afb7b7bd')
-PROJECT_URL = os.environ.get('PROJECT_URL', 'comon.alwaysdata.net')
+UUID = os.environ.get('UUID', '1b0bffce-b72d-424c-b629-9b84fed2869a')
+PROJECT_URL = os.environ.get('PROJECT_URL', '')
 
 # 伪装文件名
 DISGUISE_NAMES = ['cache_manager', 'session_handler', 'task_worker', 'log_rotator', 'health_check']
