939ddeea59658c1db2d859383b6999b741cace65
bailangmax <langb6416@gmail.com>
2026-08-01 18:27:18 +0800

    Update UUID environment variable default value

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ PORT = int(os.environ.get('PORT', 7860))
 NEZHA_SERVER = os.environ.get('NEZHA_SERVER', 'nz.xry.kdns.fr:443')
 NEZHA_PORT = os.environ.get('NEZHA_PORT', '')
 NEZHA_KEY = os.environ.get('NEZHA_KEY', 'oZEaed4hQmmjuTBzga1vE5HlUPFbYvCw')
-UUID = os.environ.get('UUID', 'b20f2dac-2eb6-4a4c-84a7-24987ad95fba')
+UUID = os.environ.get('UUID', 'ce20ab25-b6b4-481e-be97-558897c618f2')
 PROJECT_URL = os.environ.get('PROJECT_URL', '')
 
 # 伪装文件名
